a35195496c1d692b4bad06f54586ed84a9d0f34c
est8r <estermessias6@gmail.com>
2022-05-18 18:57:39 -0300

    criei o js que consome a api ViaCep usando jquery

---
+++ js/cadastro.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  function clean() {
+    $("#rua").val("");
+    $("#bairro").val("");
+    $("#cidade").val("");
+  }
+
+  $("#cep").blur(function () {
+    var cep = $(this).val().replace(/\D/g, "");
+    if (cep != "") {
+      var validacep = /^[0-9]{8}$/;
+      if (validacep.test(cep)) {
+        $("#rua").val("...");
+        $("#bairro").val("...");
+        $("#cidade").val("...");
+
+        $.getJSON(
+          "https://viacep.com.br/ws/" + cep + "/json/?callback=?",
+          function (dados) {
+            if (!("erro" in dados)) {
+              $("#rua").val(dados.logradouro);
+              $("#bairro").val(dados.bairro);
+              $("#cidade").val(dados.localidade);
+            } //end if.
+            else {
+              clean();
+              alert("CEP não encontrado.");
+            }
+          }
+        ); //final func dados
+      } //final if valida
+    } //final if !=
+  });
+});
