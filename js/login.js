c31bae0fc1d9e54ecbb72228fef6dbc4165421a7
Wellison Sales <wellisonsalesbr@gmail.com>
2022-05-23 16:46:54 -0300

    Criei a página js, 3 constantes e evento de clique

---
+++ js/login.js
@@ -0,0 +1,9 @@
+const emailInput = document.querySelector("#floatingInput")
+const passwordInput = document.querySelector("#floatingPassword")
+const submitButton = document.querySelector("#submit")
+
+submitButton.addEventListener('click' (e) => {
+    e.preventDefault()
+
+
+})
