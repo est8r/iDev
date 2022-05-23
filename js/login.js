6743b18f99222c73153715fcc72b80db17811429
Wellison Sales <wellisonsalesbr@gmail.com>
2022-05-23 17:05:33 -0300

    Adicionei validação de preenchimento incompleto

--- js/login.js
+++ js/login.js
@@ -1,9 +1,16 @@
 const emailInput = document.querySelector("#floatingInput")
 const passwordInput = document.querySelector("#floatingPassword")
 const submitButton = document.querySelector("#submit")
+const errorReport = document.querySelector("#msg")
 
-submitButton.addEventListener('click' (e) => {
-    e.preventDefault()
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
 
+    const emailValue = emailInput.value;
+    const passwordValue = passwordInput.value;
 
+    if (emailValue === '' || passwordValue === ''){
+        errorReport.textContent = "Por favor, digite todos os campos.";
+        errorReport.classList= 'error';
+    }
 })
